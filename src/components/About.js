9588a39cf9c71e461123778387516be93c9aa981
Keith Capers <K.Capers94@gmail.com>
2024-06-02 13:28:05 -0400

    complete lab

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+function About({image, about}) {
+    const defaultImage =  "https://via.placeholder.com/215"
+    
+    return (
+        <aside>
+            <img src={image || defaultImage} alt="blog logo"></img>
+            <p>{about}</p>
+        </aside>
+    )
+}
+
+
+
+export default About
